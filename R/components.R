f79a210e0709e02a0d88fadaaaf2fab6a2258c27
stevencarlislewalker <stevencarlislewalker@gmail.com>
2025-11-13 13:58:33 -0500

    stub print method for plot components

--- R/components.R
+++ R/components.R
@@ -1,1 +1,6 @@
 init_components = function() structure(list(), class = "vizid_components")
+
+#' @exportS3Method base::print
+print.vizid_components = function(x, ...) {
+  cat("No plot components have been built")
+}
